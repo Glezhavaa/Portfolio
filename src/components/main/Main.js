df185bad4ede983267217f36125027dcc72dddcb
George <glezhavaa@gmail.com>
2022-10-21 23:43:41 +0400

    typewriter

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,26 @@
 import * as styles from "./Main.module.css";
+import Typewriter from "typewriter-effect";
 
 function Main() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>HI, I'M GIORGI LEZHAVA</h1>
+        <h1>
+          Hi, I'm
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Giorgi Lezhava.")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Front-End Developer.")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Giorgi Lezhava.")
+                .start();
+            }}
+          />
+        </h1>
         <h6>
           A Frontend focused Web Developer building the Frontend of Websites and
           Web Applications that leads to the success of the overall product
